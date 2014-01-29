c5ed3fcf3956fd548415270653675c48135bbf48
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2014-01-29 08:52:54 -0800

    adding generated hs files

--- hs/lib/OpenCV/Core/MatUtil.hs
+++ hs/lib/OpenCV/Core/MatUtil.hs
@@ -1,6 +1,22 @@
 module OpenCV.Core.MatUtil where
 
-import Mat_Types
+import Thrift
+import Thrift.Protocol.Binary
+import Thrift.Transport
+import Thrift.Transport.Handle
+import Thrift.Server
 
-class MatUtil a where
-  pack :: a -> CVType -> MatUnpacked -> IO Mat 
+import Mat_Types
+import qualified MatUtil
+
+{-data MatUtilClient a = MatUtilClient a-}
+
+{-type Client = Int-}
+
+{-class MatUtilClient a where-}
+  {-cast :: a -> Client -}
+
+{-class MatUtilClient a => MatUtil a where-}
+  {-pack :: a -> CVType -> MatUnpacked -> IO Mat -}
+  {-pack client cvType matUnpacked = -}
+    {-MatUtil.pack undefined cvType matUnpacked-}
