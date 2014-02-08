c667c33e2930c97588098a481ddfe966cae33d1a
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2014-02-07 19:29:28 -0800

    changes to interface

--- interface/OpenCV/Core/Types.thrift
+++ interface/OpenCV/Core/Types.thrift
@@ -1,13 +1,14 @@
-struct Point2f {
-  required float x
-  required float y
+struct Point2d {
+  required double x
+  required double y
 }
 
 struct KeyPoint {
-  required Point2f pt
-  required float size
-  required float angle
-  required float response
+  // Thrift doesn't have 32-bit floats, so we use Point2d, not Point2f.
+  required Point2d pt
+  required double size
+  required double angle
+  required double response
   required int octave
   required int class_id
 }
