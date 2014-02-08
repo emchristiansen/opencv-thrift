edfd913176157db0ed9ef029681edf81020e510f
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2014-02-08 10:22:29 -0800

    added Ports.thrift

---
+++ interface/Ports.thrift
@@ -0,0 +1,10 @@
+/**
+ * Ports for each of the services defined for OpenCV.
+ * These will be rendered obsolete by an implementation of a multiplexing
+ * client for all client languages.
+ */
+
+const int64 basePort = 9090
+
+const int64 matUtilPortOffset = 0
+const int64 features2DPortOffset = 1
