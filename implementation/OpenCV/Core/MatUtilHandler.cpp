7ace65379706f624f51cbcbe5c6070821f0b804c
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2014-02-07 17:42:44 -0800

    filling out implementation, changed some type names

--- implementation/OpenCV/Core/MatUtilHandler.cpp
+++ implementation/OpenCV/Core/MatUtilHandler.cpp
@@ -1,6 +1,14 @@
 #include "OpenCV/Core/MatUtilHandler.hpp"
 
+#include <opencv2/core/core.hpp>
+
 #include "OpenCV/Core/MatUtil.h"
+
+int cvTypeToCVMacro(const CVType::type cvType) {
+}
+
+CVType::type cvMacroToCVType(const int cvMacro) {
+}
 
 MatUtilHandler::MatUtilHandler() {
   // Your initialization goes here
@@ -9,6 +17,26 @@ MatUtilHandler::MatUtilHandler() {
 void MatUtilHandler::pack( ::Mat& _return, const  ::CVType::type type, const  ::MatUnpacked& matUnpacked) {
   // Your implementation goes here
   printf("pack\n");
+
+  CVType::type unpackedType;
+  switch(matUnpacked.channels) {
+    case 1: 
+      unpackedType = CVType::type::T64FC1;
+      break;
+    case 2: 
+      unpackedType = CVType::type::T64FC2;
+      break;
+    case 3: 
+      unpackedType = CVType::type::T64FC2;
+      break;
+    case 4: 
+      unpackedType = CVType::type::T64FC4;
+      break;
+    default: 
+      assert(false);
+  }
+
+  cv::Mat cvUnpacked(matUnpacked.rows, matUnpacked.cols, cvTypeToCVMacro(unpackedType));
 }
 
 void MatUtilHandler::unpack( ::MatUnpacked& _return, const  ::Mat& mat) {
