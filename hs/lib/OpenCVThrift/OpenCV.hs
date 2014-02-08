362a3be09c7abd9244d5794446e3be269c9980f1
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2014-02-08 11:54:54 -0800

    renamed bin to examples, some build config changes

--- hs/lib/OpenCVThrift/OpenCV.hs
+++ hs/lib/OpenCVThrift/OpenCV.hs
@@ -1,43 +1,10 @@
 module OpenCV where
 
-{-import qualified MatUtil_Client-}
-{-import qualified Features2D_Client-}
-{-import OpenCVTypes-}
-{-import MatUtil_Iface-}
-
-{-import Thrift-}
-{-import Thrift.Protocol.Binary-}
-{-import Thrift.Transport-}
-{-import Thrift.Transport.Handle-}
-{-import Thrift.Server-}
-
 import Thrift.Protocol
 import Thrift.Transport
 
 import qualified OpenCV.Core.MatUtil
 import qualified OpenCV.Features2D.Features2D
-
--- We're assuming all sub-clients use the same protocol and transport types.
-{-data OpenCVClient mu f2d p t = -}
-  {-( OpenCV.Core.MatUtil.Client mu p t-}
-  {-, OpenCV.Features2D.Features2D.Client f2d p t) => OpenCVClient-}
-    {-{ matUtil :: mu-}
-    {-, features2D :: f2d-}
-    {-}-}
-
-{-instance-}
-  {-( Thrift.Protocol.Protocol p-}
-  {-, Thrift.Transport.Transport t) => -}
-    {-OpenCV.Core.MatUtil.Client (OpenCVClient mu f2d p t) p t where-}
-      {-cast OpenCVClient { matUtil = client } = -}
-        {-OpenCV.Core.MatUtil.cast client-}
-
-{-instance-}
-  {-( Thrift.Protocol.Protocol p-}
-  {-, Thrift.Transport.Transport t) => -}
-    {-OpenCV.Features2D.Features2D.Client (OpenCVClient mu f2d p t) p t where-}
-      {-cast OpenCVClient { features2D = client } = -}
-        {-OpenCV.Features2D.Features2D.cast client-}
 
 data Client p t = Client 
   { matUtil :: OpenCV.Core.MatUtil.Client p t
